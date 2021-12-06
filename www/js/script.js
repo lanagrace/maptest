a4d817239c1e562de69168e1bbe721dd5e2fe52b
lanagrace <lapericic@gmail.com>
2021-12-06 19:56:29 +0000

    seed and soil

--- www/js/script.js
+++ www/js/script.js
@@ -1,14 +1,27 @@
 document.addEventListener('DOMContentLoaded', ()=>{
-    var map = L.map('map').setView([50.37566179067675, -4.139420986175538], 13);
+    var map = L.map('map').setView([50.3179372480405, -4.159926715660778], 13);
 
         L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
             attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-            maxZoom: 18,
+            maxZoom: 24,
             id: 'mapbox/streets-v11',
             tileSize: 512,
             zoomOffset: -1,
-            accessToken: 'pk.eyJ1IjoibHBlcmljaWMiLCJhIjoiY2tvanVhOWU3MGFiazJ3cWt2YjV5am53OSJ9.99cYacURj_XngBppBaIhmw'
+            accessToken: 'pk.eyJ1IjoibHBlcmljaWMiLCJhIjoiY2t3dXdhdm9nMXI4djJvcnQ0dGcxZWdlOCJ9.V1dldbw1cZwjui6JErxfZw'
         }).addTo(map);
+
+        var marker = L.marker([50.371985264622715, -4.149493215372787]).addTo(map);
+        var marker = L.marker([50.377114394676646, -4.141468046000228]).addTo(map);
+        var marker = L.marker([50.375017920446965, -4.139468194625807]).addTo(map);
+        var marker = L.marker([50.37182103385094, -4.174512861063705]).addTo(map);
+        var marker = L.marker([50.38424608636065, -4.105247505940497]).addTo(map);
+        var marker = L.marker([50.36294088192618, -4.09566879272461]).addTo(map);
+        var marker = L.marker([50.38766103029909, -4.1665306070353845]).addTo(map);
+        var marker = L.marker([50.36272186653412, -4.1612777626141915]).addTo(map);
+        var marker = L.marker([50.412018243343766, -4.119907381245867]).addTo(map);
+        var marker = L.marker([50.40219370493006, -4.138000484090299]).addTo(map);
+
+
 
         getPins();
 
